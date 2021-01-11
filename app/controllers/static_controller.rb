988a05f620cdf2efb704d9e2d167b0b747a9eb9c
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-01-11 15:47:39 -0500

    commit tree 5ecbeba3de8170b55a32227c38b93a9b2ff5f4e6

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,8 @@
+class StaticController < ApplicationController
+
+
+    def about 
+        # render "static/some_page"
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'static#about'
 end
+
+# The HTTP verb - in this case we're using the get HTTP verb.
+# The path - 'about' represents the path in the URL bar that the route will be mapped to.
+
+# The controller action - 'static#about' tells the Rails routing system that this route should be passed through the static controller's about action. If the term action sounds foreign, actions are just Ruby speak for a method in a controller. So in the StaticController will be a method called about that gets called when a user goes to /about.
